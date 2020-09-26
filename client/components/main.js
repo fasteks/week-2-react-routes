3df0995151a0c43623138a0e4c517fa304669dec
fasteks <fastik-07@mail.ru>
2020-09-27 00:48:53 +0300

    task 2 solution v1

--- client/components/main.js
+++ client/components/main.js
@@ -3,15 +3,23 @@ import { Link } from 'react-router-dom'
 
 import Header from './header'
 
-const Home = () => {
+const Main = () => {
   return (
     <div>
-      <Header />
+      <a
+        role="button"
+        aria-label="Go to google link"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.google.com/"
+      >
+        <Header />
+      </a>
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">Dashboard</div>
+          <div id="title">Main</div>
           <div>
-            <Link to="/dashboard/main">Go To Main</Link>
+            <Link to="/dashboard">Go To Root</Link>
           </div>
           <div>
             <Link to="/dashboard/profile/71cfff71-34e1-46eb-95ad-29637d913771">Go To Profile</Link>
@@ -22,6 +30,6 @@ const Home = () => {
   )
 }
 
-Home.propTypes = {}
+Main.propTypes = {}
 
-export default React.memo(Home)
+export default Main
--- client/components/user.js
+++ client/components/user.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 import Header from './header'
 
-const Home = () => {
+const User = () => {
+  const { user } = useParams()
   return (
     <div>
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">Dashboard</div>
+          <div id="title">Profile</div>
+          <div>
+            <Link to="/dashboard">Go To Root</Link>
+          </div>
           <div>
             <Link to="/dashboard/main">Go To Main</Link>
           </div>
-          <div>
-            <Link to="/dashboard/profile/71cfff71-34e1-46eb-95ad-29637d913771">Go To Profile</Link>
-          </div>
+          <div id={user}>{user}</div>
         </div>
       </div>
     </div>
   )
 }
 
-Home.propTypes = {}
+User.propTypes = {}
 
-export default React.memo(Home)
+export default User
