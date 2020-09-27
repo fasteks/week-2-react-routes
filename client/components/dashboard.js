3a1b24bbeb93a712cfcb18dbdf1e9f87121f2465
fasteks <fastik-07@mail.ru>
2020-09-27 22:59:27 +0300

    moved link to the central div

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div>
-      <div id="title">Dashboard</div>
-      <div>
-        <Link to="/dashboard/profile/71cfff71-34e1-46eb-95ad-29637d913771">Go To Profile</Link>
-      </div>
-      <div>
-        <Link to="/dashboard/main">Go To Main</Link>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">Dashboard</div>
+          <div>
+            <Link to="/dashboard/profile/71cfff71-34e1-46eb-95ad-29637d913771">Go To Profile</Link>
+          </div>
+          <div>
+            <Link to="/dashboard/main">Go To Main</Link>
+          </div>
+        </div>
       </div>
     </div>
   )
--- client/components/main.js
+++ client/components/main.js
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom'
 const Main = () => {
   return (
     <div>
-      <div id="title">Main</div>
-      <div>
-        <Link to="/dashboard/profile/71cfff71-34e1-46eb-95ad-29637d913771">Go To Profile</Link>
-      </div>
-      <div>
-        <Link to="/dashboard">Go To Root</Link>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">Main</div>
+          <div>
+            <Link to="/dashboard/profile/71cfff71-34e1-46eb-95ad-29637d913771">Go To Profile</Link>
+          </div>
+          <div>
+            <Link to="/dashboard">Go To Root</Link>
+          </div>
+        </div>
       </div>
     </div>
   )
